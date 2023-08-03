15949bc08c3c0389dc1b3862cd2c17ddb13aa845
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-08-03 16:49:09 +0530

    Add files via upload

---
+++ indicator1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void swap(int *a,int *b)
+{
+	int c;
+	 c=*a;
+	*a=*b;
+	 *b=c;
+	printf(" swap than A :%d \n",*a);
+	printf(" SWAP than B :%d \n",*b);
+}
+int main()
+{
+	int a,b;
+		
+	printf("ENTER A = ");
+	scanf("%d",&a);
+		
+	printf("ENTER B = ");
+	scanf("%d",&b);
+	swap(&a,&b);
+}
